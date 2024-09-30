69e7c3b839ab0817f6fffaf0a50e028587f93423
Matt Brewer <Matt@Matts-MacBook-Pro-2.local>
2024-09-29 23:19:19 -0400

    Working V1

--- program/func_cointegration.py
+++ program/func_cointegration.py
@@ -6,94 +6,86 @@ from scipy.stats import linregress
 from constants import MAX_HALF_LIFE, WINDOW
 
 class SmartError(Exception):
-    pass
+  pass
 
-# Function to calculate the half-life of mean reversion
 def half_life_mean_reversion(series):
-    if len(series) <= 1:
-        raise SmartError("Series length must be greater than 1.")
-    difference = np.diff(series)
-    lagged_series = series[:-1]
-    slope, _, _, _, _ = linregress(lagged_series, difference)
-    if np.abs(slope) < np.finfo(np.float64).eps:
-        raise SmartError("Cannot calculate half life. Slope value is too close to zero.")
-    half_life = -np.log(2) / slope
-    return half_life
+  if len(series) <= 1:
+    raise SmartError("Series length must be greater than 1.")
+  difference = np.diff(series)
+  lagged_series = series[:-1]
+  slope, _, _, _, _ = linregress(lagged_series, difference)
+  if np.abs(slope) < np.finfo(np.float64).eps:
+    raise SmartError("Cannot calculate half life. Slope value is too close to zero.")
+  half_life = -np.log(2) / slope
+  return half_life
+
 
 # Calculate ZScore
 def calculate_zscore(spread):
-    spread_series = pd.Series(spread)
-    mean = spread_series.rolling(center=False, window=WINDOW).mean()
-    std = spread_series.rolling(center=False, window=WINDOW).std()
-    x = spread_series.rolling(center=False, window=1).mean()
-    zscore = (x - mean) / std
-    return zscore
+  spread_series = pd.Series(spread)
+  mean = spread_series.rolling(center=False, window=WINDOW).mean()
+  std = spread_series.rolling(center=False, window=WINDOW).std()
+  x = spread_series.rolling(center=False, window=1).mean()
+  zscore = (x - mean) / std
+  return zscore
 
-# Function to calculate the cointegration between two series
+
+# Calculate Cointegration
 def calculate_cointegration(series_1, series_2):
-    series_1 = np.array(series_1).astype(np.float64)
-    series_2 = np.array(series_2).astype(np.float64)
-    coint_flag = 0
-    hedge_ratio = None
-    half_life = None
+  series_1 = np.array(series_1).astype(np.float64)
+  series_2 = np.array(series_2).astype(np.float64)
+  coint_flag = 0
+  coint_res = coint(series_1, series_2)
+  coint_t = coint_res[0]
+  p_value = coint_res[1]
+  critical_value = coint_res[2][1]
 
-    # Skip if series length is insufficient
-    if len(series_1) <= 1 or len(series_2) <= 1:
-        print(f"Skipping pair due to insufficient data: series_1 length: {len(series_1)}, series_2 length: {len(series_2)}")
-        return coint_flag, hedge_ratio, half_life
+  # Better way to fit data vs older version
+  series_2_with_constant = sm.add_constant(series_2) 
+  model = sm.OLS(series_1, series_2_with_constant).fit()
+  hedge_ratio = model.params[1]
+  intercept = model.params[0]
 
-    try:
-        coint_res = coint(series_1, series_2)
-        coint_t = coint_res[0]
-        p_value = coint_res[1]
-        critical_value = coint_res[2][1]
+  spread = series_1 - (series_2 * hedge_ratio) - intercept
+  half_life = half_life_mean_reversion(spread)
+  t_check = coint_t < critical_value
+  coint_flag = 1 if p_value < 0.05 and t_check else 0
+  return coint_flag, hedge_ratio, half_life
 
-        # Fit the model
-        series_2_with_constant = sm.add_constant(series_2)
-        model = sm.OLS(series_1, series_2_with_constant).fit()
-        hedge_ratio = model.params[1]
-        intercept = model.params[0]
-
-        spread = series_1 - (series_2 * hedge_ratio) - intercept
-        half_life = half_life_mean_reversion(spread)
-        t_check = coint_t < critical_value
-        coint_flag = 1 if p_value < 0.05 and t_check else 0
-    except Exception as e:
-        print(f"Error in cointegration calculation: {e}")
-
-    return coint_flag, hedge_ratio, half_life
 
 # Store Cointegration Results
 def store_cointegration_results(df_market_prices):
-    markets = df_market_prices.columns.to_list()
-    criteria_met_pairs = []
 
-    # Iterate over the pairs of markets
-    for index, base_market in enumerate(markets[:-1]):
-        series_1 = df_market_prices[base_market].values.astype(np.float64).tolist()
+  # Initialize
+  markets = df_market_prices.columns.to_list()
+  criteria_met_pairs = []
 
-        for quote_market in markets[index + 1:]:
-            series_2 = df_market_prices[quote_market].values.astype(np.float64).tolist()
+  # Find cointegrated pairs
+  # Start with our base pair
+  for index, base_market in enumerate(markets[:-1]):
+    series_1 = df_market_prices[base_market].values.astype(np.float64).tolist()
 
-            # Calculate cointegration
-            coint_flag, hedge_ratio, half_life = calculate_cointegration(series_1, series_2)
+    # Get Quote Pair
+    for quote_market in markets[index +1:]:
+      series_2 = df_market_prices[quote_market].values.astype(np.float64).tolist()
 
-            # Log the pair if it meets the criteria
-            if coint_flag == 1 and half_life is not None and half_life <= MAX_HALF_LIFE and half_life > 0:
-                criteria_met_pairs.append({
-                    "base_market": base_market,
-                    "quote_market": quote_market,
-                    "hedge_ratio": hedge_ratio,
-                    "half_life": half_life,
-                })
+      # Check cointegration
+      coint_flag, hedge_ratio, half_life = calculate_cointegration(series_1, series_2)
 
-    # Save the results to a CSV file
-    if len(criteria_met_pairs) > 0:
-        df_criteria_met = pd.DataFrame(criteria_met_pairs)
-        df_criteria_met.to_csv("cointegrated_pairs.csv")
-        del df_criteria_met
-        print("Cointegrated pairs successfully saved")
-    else:
-        print("No cointegrated pairs met the criteria")
+      # Log pair
+      if coint_flag == 1 and half_life <= MAX_HALF_LIFE and half_life > 0:
+        criteria_met_pairs.append({
+          "base_market": base_market,
+          "quote_market": quote_market,
+          "hedge_ratio": hedge_ratio,
+          "half_life": half_life,
+        })
 
-    return "saved"
+  # Create and save DataFrame
+  df_criteria_met = pd.DataFrame(criteria_met_pairs)
+  df_criteria_met.to_csv("cointegrated_pairs.csv")
+  del df_criteria_met
+
+  # Return result
+  print("Cointegrated pairs successfully saved")
+  return "saved"
--- program/func_public.py
+++ program/func_public.py
@@ -4,137 +4,120 @@ import pandas as pd
 import numpy as np
 import time
 
+from pprint import pprint
+
 # Get relevant time periods for ISO from and to
 ISO_TIMES = get_ISO_times()
 
 # Get Recent Candles
 async def get_candles_recent(client, market):
 
-    # Define output
-    close_prices = []
+  # Define output
+  close_prices = []
 
-    # Protect API
-    time.sleep(0.2)
+  # Protect API
+  time.sleep(0.2)
 
-    try:
-        # Get Prices from DYDX V4
-        response = await client.indexer.markets.get_perpetual_market_candles(
-            market=market,
-            resolution=RESOLUTION
-        )
+  # Get Prices from DYDX V4
+  response = await client.indexer.markets.get_perpetual_market_candles(
+    market = market, 
+    resolution = RESOLUTION
+  )
 
-        # Check response validity
-        if "candles" not in response:
-            print(f"Error: No candles found for market {market}")
-            return np.array([])
+  # Candles
+  candles = response
 
-        # Structure data
-        for candle in response["candles"]:
-            close_prices.append(candle["close"])
+  # Structure data
+  for candle in candles["candles"]:
+    close_prices.append(candle["close"])
 
-        # Construct and return close price series
-        close_prices.reverse()
-        return np.array(close_prices).astype(np.float64)
+  # Construct and return close price series
+  close_prices.reverse()
+  prices_result = np.array(close_prices).astype(np.float64)
+  return prices_result
 
-    except Exception as e:
-        print(f"Error fetching candles for market {market}: {e}")
-        return np.array([])
 
 # Get Historical Candles
 async def get_candles_historical(client, market):
 
-    # Define output
-    close_prices = []
+  # Define output
+  close_prices = []
 
-    # Extract historical price data for each timeframe
-    for timeframe in ISO_TIMES.keys():
-        tf_obj = ISO_TIMES[timeframe]
-        from_iso = tf_obj["from_iso"] + ".000Z"
-        to_iso = tf_obj["to_iso"] + ".000Z"
+  # Extract historical price data for each timeframe
+  for timeframe in ISO_TIMES.keys():
 
-        # Protect rate limits
-        time.sleep(0.2)
+    # Confirm times needed
+    tf_obj = ISO_TIMES[timeframe]
+    from_iso = tf_obj["from_iso"] + ".000Z"
+    to_iso = tf_obj["to_iso"] + ".000Z"
 
-        try:
-            response = await client.indexer.markets.get_perpetual_market_candles(
-                market=market,
-                resolution=RESOLUTION,
-                from_iso=from_iso,
-                to_iso=to_iso,
-                limit=100
-            )
+    # Protect rate limits
+    time.sleep(0.2)
 
-            if "candles" not in response:
-                print(f"Error: No historical candles found for market {market} during {timeframe}")
-                continue
+    response = await client.indexer.markets.get_perpetual_market_candles(
+      market = market, 
+      resolution = RESOLUTION, 
+      from_iso = from_iso,
+      to_iso = to_iso,
+      limit = 100
+    )
 
-            # Structure data
-            for candle in response["candles"]:
-                close_prices.append({"datetime": candle["startedAt"], market: candle["close"]})
+    candles = response
 
-        except Exception as e:
-            print(f"Error fetching historical candles for market {market} during {timeframe}: {e}")
-            continue
+    # Structure data
+    for candle in candles["candles"]:
+      close_prices.append({"datetime": candle["startedAt"], market: candle["close"] })
 
-    # Construct and return DataFrame
-    close_prices.reverse()
-    return close_prices
+  # Construct and return DataFrame
+  close_prices.reverse()
+  return close_prices
+
 
 # Get Markets
 async def get_markets(client):
-    try:
-        return await client.indexer.markets.get_perpetual_markets()
-    except Exception as e:
-        print(f"Error fetching markets: {e}")
-        return {}
+  return await client.indexer.markets.get_perpetual_markets()
+
 
 # Construct market prices
 async def construct_market_prices(client):
 
-    # Ensure only Testnet Assets are used
-    tradeable_markets = []
-    markets = await get_markets(client)
+  # Ensure only Testnet Assets are used
 
-    if "markets" not in markets:
-        print("Error: Unable to fetch markets")
-        return pd.DataFrame()  # Return empty DataFrame on failure
+  # Declare variables
+  tradeable_markets = []
+  markets = await get_markets(client)
 
-    # Find tradeable pairs
-    for market in markets["markets"].keys():
-        market_info = markets["markets"][market]
-        if market_info["status"] == "ACTIVE":
-            tradeable_markets.append(market)
+  # Find tradeable pairs
+  for market in markets["markets"].keys():
+    market_info = markets["markets"][market]
+    if market_info["status"] == "ACTIVE":
+      tradeable_markets.append(market)
 
-    # Set initial DataFrame
-    close_prices = await get_candles_historical(client, tradeable_markets[0])
-    if len(close_prices) == 0:
-        print("Error: No historical data found for the first market")
-        return pd.DataFrame()  # Return empty DataFrame on failure
+  # Set initial DateFrame
+  close_prices = await get_candles_historical(client, tradeable_markets[0])
+  df = pd.DataFrame(close_prices)
+  df.set_index("datetime", inplace=True)
 
-    df = pd.DataFrame(close_prices)
-    df.set_index("datetime", inplace=True)
+  # Append other prices to DataFrame
+  # You can limit the amount to loop though here to save time in development
+  for (i, market) in enumerate(tradeable_markets[0:]):
+    print(f"Extracting prices for {i + 1} of {len(tradeable_markets)} tokens for {market}")
+    close_prices_add = await get_candles_historical(client, market)
+    df_add = pd.DataFrame(close_prices_add)
+    try:
+      df_add.set_index("datetime", inplace=True)
+      df = pd.merge(df, df_add, how="outer", on="datetime", copy=False)
+      # print(df.head())
+    except Exception as e: 
+      print(f"Failed to add {market} - {e}")
+    del df_add
 
-    # Append other prices to DataFrame
-    for (i, market) in enumerate(tradeable_markets[1:]):  # Corrected to skip the first market
-        print(f"Extracting prices for {i + 1} of {len(tradeable_markets)} tokens for {market}")
-        close_prices_add = await get_candles_historical(client, market)
-        if len(close_prices_add) == 0:
-            print(f"Error: No data found for {market}, skipping...")
-            continue
+  # Check any columns with NaNs
+  nans = df.columns[df.isna().any()].tolist()
+  if len(nans) > 0:
+    print("Dropping columns: ")
+    print(nans)
+    df.drop(columns=nans, inplace=True)
 
-        df_add = pd.DataFrame(close_prices_add)
-        try:
-            df_add.set_index("datetime", inplace=True)
-            df = pd.merge(df, df_add, how="outer", on="datetime", copy=False)
-        except Exception as e:
-            print(f"Failed to add {market} - {e}")
-        del df_add
-
-    # Check for columns with NaNs
-    nans = df.columns[df.isna().any()].tolist()
-    if len(nans) > 0:
-        print(f"Dropping columns with NaNs: {nans}")
-        df.drop(columns=nans, inplace=True)
-
-    # Return result
-    return df
+  # Return result
+  return df
