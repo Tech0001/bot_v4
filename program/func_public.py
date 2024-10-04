5c1e8b5e07f78881b6e7533da55a9cac83db0908
Matt Brewer <Matt@Matts-MacBook-Pro-2.local>
2024-10-04 11:54:38 -0400

    Update with latest code from VS

--- program/func_public.py
+++ program/func_public.py
@@ -11,7 +11,7 @@ import json
 async def cancel_order(client, order_id):
     order = await get_order(client, order_id)
     ticker = order["ticker"]
-    market = Market((await client.indexer.markets.get_perpetual_markets(ticker))["markets"][ticker])
+    market = Market((await client.indexer.markets.get_perpetual_markets())["markets"][ticker])
     market_order_id = market.order_id(DYDX_ADDRESS, 0, random.randint(0, MAX_CLIENT_ID), OrderFlags.SHORT_TERM)
     current_block = await client.node.latest_block_height()
     good_til_block = current_block + 1 + 10
@@ -24,24 +24,24 @@ async def cancel_order(client, order_id):
 
 # Get Order
 async def get_order(client, order_id):
-    return await client.indexer_account.account.get_order(order_id)
+    return await client.indexer_account.orders.get_order_by_id(order_id)
 
 # Get Account
 async def get_account(client):
-    account = await client.indexer_account.account.get_subaccount(DYDX_ADDRESS, 0)
-    return account["subaccount"]
+    account = await client.indexer_account.accounts.get_by_address(DYDX_ADDRESS)
+    return account["account"]
 
 # Get Open Positions
 async def get_open_positions(client):
-    response = await client.indexer_account.account.get_subaccount(DYDX_ADDRESS, 0)
-    return response["subaccount"]["openPerpetualPositions"]
+    response = await client.indexer_account.positions.get_positions_by_account(DYDX_ADDRESS)
+    return response["positions"]
 
 # Check if Positions are Open
 async def is_open_positions(client, market):
     time.sleep(0.2)
-    response = await client.indexer_account.account.get_subaccount(DYDX_ADDRESS, 0)
-    open_positions = response["subaccount"]["openPerpetualPositions"]
-    return market in open_positions.keys()
+    response = await client.indexer_account.positions.get_positions_by_account(DYDX_ADDRESS)
+    open_positions = response["positions"]
+    return any(pos['market'] == market for pos in open_positions)
 
 # Place Market Order
 async def place_market_order(client, market, side, size, price, reduce_only):
@@ -51,7 +51,7 @@ async def place_market_order(client, market, side, size, price, reduce_only):
 
         ticker = market
         current_block = await client.node.latest_block_height()
-        market = Market((await client.indexer.markets.get_perpetual_markets(market))["markets"][market])
+        market = Market((await client.indexer.markets.get_perpetual_markets())["markets"][market])
         market_order_id = market.order_id(DYDX_ADDRESS, 0, random.randint(0, MAX_CLIENT_ID), OrderFlags.SHORT_TERM)
         good_til_block = current_block + 1 + 10
 
@@ -77,7 +77,7 @@ async def place_market_order(client, market, side, size, price, reduce_only):
 
 # Cancel all open orders
 async def cancel_all_orders(client):
-    orders = await client.indexer_account.account.get_subaccount_orders(DYDX_ADDRESS, 0, status="OPEN")
+    orders = await client.indexer_account.orders.get_orders_by_account(DYDX_ADDRESS)
     if len(orders) > 0:
         for order in orders:
             await cancel_order(client, order["id"])
@@ -94,8 +94,7 @@ async def abort_all_positions(client):
     positions = await get_open_positions(client)
 
     if len(positions) > 0:
-        for item in positions.keys():
-            pos = positions[item]
+        for pos in positions:
             market = pos["market"]
             side = "BUY" if pos["side"] == "SHORT" else "SELL"
             price = float(pos["entryPrice"])
@@ -103,7 +102,7 @@ async def abort_all_positions(client):
             tick_size = markets["markets"][market]["tickSize"]
             accept_price = format_number(accept_price, tick_size)
 
-            result = await place_market_order(client, market, side, pos["sumOpen"], accept_price, True)
+            result = await place_market_order(client, market, side, pos["openSize"], accept_price, True)
 
             if result is None:
                 print(f"Error closing position for {market}")
@@ -128,7 +127,7 @@ async def get_markets(client):
 async def get_candles_recent(client, market):
     """ Function to fetch recent candle data for a specific market. """
     try:
-        candles = await client.indexer.markets.get_market_candles(market)
+        candles = await client.indexer.markets.get_candles_by_market(market)
         return candles
     except Exception as e:
         print(f"Error fetching candles for {market}: {e}")
