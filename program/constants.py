285da24bb6bb2d3482d9c329807b5df303c92e6c
Matt Brewer <Matt@Matts-MacBook-Pro-2.local>
2024-10-08 21:15:26 -0400

    Update with latest code from VS

--- program/constants.py
+++ program/constants.py
@@ -23,7 +23,7 @@ SPECIFIC_PAIRS = [
 MARKET_DATA_MODE = "TESTNET" # vs "MAINNET"
 
 # Close all open positions and orders
-ABORT_ALL_POSITIONS = False
+ABORT_ALL_POSITIONS = True
 
 # Find Cointegrated Pairs
 FIND_COINTEGRATED = False
